a6936cb870129ef763ce756c16f8a97cd5822de7
maye1995 <maye199512@gmail.com>
2016-10-15 21:50:32 +0800

    新增数据库帮助类，以及各表对应的实体类。

--- app/src/main/java/app/coolweather/com/coolweather/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/app/coolweather/com/coolweather/db/CoolWeatherOpenHelper.java
@@ -18,6 +18,26 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
             +"id integer primary key autoincrement,"
             +"province_name text,"
             +"province_code text)";
+
+    /*
+    * City 建表语句
+     */
+
+    public static final String CREATE_CITY = "create table City("
+            +"id integer primary key autoincrement,"
+            +"city_name text, "
+            +"city_code text, "
+            +"province_id integer)";
+
+    /*
+    * Country 建表语句
+     */
+
+    public static final String CREATE_COUNTRY = "create table Country("
+            +"id integer primary key autoincrement,"
+            +"country_name text, "
+            +"country_code text, "
+            +"city_id integer)";
     public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -28,7 +48,9 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTRY);
     }
 
     @Override
---
+++ app/src/main/java/app/coolweather/com/coolweather/model/City.java
@@ -0,0 +1,44 @@
+package app.coolweather.com.coolweather.model;
+
+/**
+ * Created by lenovo on 2016/10/15.
+ */
+
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
